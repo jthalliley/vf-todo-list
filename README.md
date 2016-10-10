40cb71bb9880e8f9f93d36b5444d1a2e99d24161
Tom Halliley <tomh@acm.org>
2016-10-10 14:18:39 -0400

    Updated readme and tests script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # vf-todo-list
+
+To run on a Mac/Linux system:
+
+1. In one terminal window, do `./run.sh`
+2. To run tests, wait until the server is up. You'll see something like this:
+```
+2016-10-10 14:12:33.905  INFO 23539 --- [           main] com.vf.todo.Application                  : Started Application in 7.566 seconds (JVM running for 8.043)
+```
+  then in another terminal window, do `./tests.sh`
+
--- tests.sh
+++ tests.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-echo "Expect task one and task two"
+echo "#-- Expect task one and task two"
 curl -X GET localhost:8080/task
 
-echo "Expect 2"
+echo "#-- Expect 2"
 curl -X GET localhost:8080/countTasks
 
 echo
-echo "add another task"
+echo "#-- Add another task"
 curl -H "Content-Type: application/json" -X PUT -d '{"title":"command line task","status":"ACTIVE"}' localhost:8080/task
 
 echo
-echo "Expect task one and task two and command line task"
+echo "#-- Expect task one and task two and command line task"
 curl -X GET localhost:8080/task
 
 echo
-echo "delete task one"
+echo "#-- Delete task one"
 curl -H "Content-Type: application/json" -X DELETE localhost:8080/task/1
 
 echo
-echo "Expect task two and command line task"
+echo "#-- Expect task two and command line task"
 curl -X GET localhost:8080/task
 
 echo
-echo "list completed tasks (none)"
+echo "#-- List completed tasks (none)"
 curl -X GET -H "Content-Type: application/json" localhost:8080/task/COMPLETED
 
 echo
-echo "set status to completed"
+echo "#-- Set status to completed"
 curl -X PUT -H "Content-Type: application/json" localhost:8080/task/2/COMPLETED
 
 echo
-echo "list completed tasks (task two)"
+echo "#-- List completed tasks (task two)"
 curl -X GET -H "Content-Type: application/json" localhost:8080/task/COMPLETED
 
 echo
-echo "delete all completed (task two)"
+echo "#-- Delete all completed (task two)"
 curl -H "Content-Type: application/json" -X DELETE localhost:8080/completedTasks
 
 echo
-echo "list completed tasks (none)"
+echo "#-- List completed tasks (none)"
 curl -X GET -H "Content-Type: application/json" localhost:8080/task/COMPLETED
 
 
